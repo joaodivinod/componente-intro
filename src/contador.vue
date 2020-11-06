477818b3cfd682c51e8259b4e97f1219fa2e5181
João Divino <joaodivinod@gmail.com>
2020-11-06 14:03:55 -0300

    Segunda sobre criação de componentes finalizada

--- src/contador.vue
+++ src/contador.vue
@@ -1,13 +1,46 @@
 <template>
-
+  <div class="contador">
+    <span>{{contador}}</span>
+    <button @click="adicionar()">+</button>
+    <button @click="subtrair()">-</button>
+  </div>
 </template>
 
 <script>
 export default {
-name: "contador"
+name: "contador",
+  data(){
+    return{
+      contador:0
+    }
+  },
+  methods:{
+    adicionar(){
+      this.contador +=1;
+    },
+    subtrair(){
+      this.contador -=1;
+    }
+  },
+
+
 }
 </script>
 
 <style scoped>
-
+  .contador span{
+    border-bottom: 1px solid #ccc;
+    height: 30px;
+    padding: 5px 25px;
+  }
+  .contador button{
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color:coral;
+    color: #fff;
+    margin-left: 10px;
+    outline: none;
+    cursor: pointer;
+  }
 </style>
